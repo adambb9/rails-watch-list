2cc5fd569e59bcaba63823055b7a08f2f0d4c2e6
Adam Biedka Whitton <adambb914@gmail.com>
2022-02-17 14:27:09 +0100

    complete

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,17 +1,29 @@
 class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
   def create
     @bookmark = Bookmark.new(bookmark_params)
-
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
     if @bookmark.save
-      redirect_to bookmark_path(@bookmark)
+      redirect_to list_path(@list)
     else
       render :new
     end
   end
 
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to lists_path
+  end
+
   private
 
-  def list_params
+  def bookmark_params
     params.require(:bookmark).permit(:comment, :list_id, :movie_id)
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
+  #attr_accessor :title, :overview, :poster_url, :rating
 
   validates :title, :overview, presence: true
   validates :title, uniqueness: true
