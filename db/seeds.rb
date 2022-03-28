300f935e81d234eea892fc2d6875a7c7df978af8
Adam Biedka Whitton <adambb914@gmail.com>
2022-03-28 21:26:33 +0200

    seeds fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,21 +9,19 @@
 require 'json'
 require 'open-uri'
 
-#Movie.destroy_all
+Movie.destroy_all
 
 puts "DB Cleaned"
 
-#Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-#Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-#Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-#Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
 
-url = "https://api.themoviedb.org/3/movie/top_rated?api_key=85a82b2fa01bce0b77d430c48db47f86&language=en-US&page=1"
-serialized = URI.open(url).read
-parsed_json = JSON.parse(serialized)
-results = parsed_json['results']
-
-p results[0]
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+490.times do |i|
+  puts "Importing movies from page #{i + 1}"
+  results = JSON.parse(URI.open("#{url}?page=#{i + 1}").read)['results']
+  results.each do |result|
+    Movie.create(title: result['original_title'], overview: result['overview'], poster_url: "https://image.tmdb.org/t/p/original/#{result['poster_path']}", rating: result['vote_average'])
+  end
+end
 
 
 
